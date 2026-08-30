d54bd87b0e840350a75a175aa2a6afc071607dd1
paes2005-design <paes2005@gmail.com>
2026-08-30 19:12:57 -0300

    Atualizar diagnóstico para build 20260830.8 e SW78

--- runtime-build-info.js
+++ runtime-build-info.js
@@ -3,10 +3,10 @@
   const INFO=Object.freeze({
     app:'ADM',
     appVersion:'1.0.0',
-    build:'20260830.7',
+    build:'20260830.8',
     htmlVersion:'index-ADMIN-v8',
     rulesModuleVersion:'6',
-    expectedServiceWorkerVersion:'77'
+    expectedServiceWorkerVersion:'78'
   });
   window.ROTINA_BUILD_INFO=INFO;
   const emit=(event,details={})=>{try{window.rotinaLog?.(event,{...INFO,...details});}catch{}};
