d85f398bab3614dac015ab3876295f74b0c6431e
paes2005-design <paes2005@gmail.com>
2026-08-31 18:52:46 -0300

    Identificar build de rollback da justificativa

--- runtime-build-info.js
+++ runtime-build-info.js
@@ -3,7 +3,7 @@
   const INFO=Object.freeze({
     app:'ADM',
     appVersion:'1.0.0',
-    build:'20260830.8',
+    build:'20260831.1',
     htmlVersion:'index-ADMIN-v8',
     rulesModuleVersion:'6',
     expectedServiceWorkerVersion:'78'
